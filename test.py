ae4f9f943091faa1f2cf78e6ae1e0eaf96bc44db
songuyenerza <sonnguyen222k@gmail.com>
2022-10-14 14:21:34 +0700

    1410_update

--- test.py
+++ test.py
@@ -16,16 +16,16 @@ from pynvml.smi import nvidia_smi
 nvsmi = nvidia_smi.getInstance()
 
 def convert_box(box, img_width, img_height):
-    x0 = int((box[0] - ((box[2]) / 2)) * img_width)
-    y0 = int((box[1] - ((box[3]) / 2)) * img_height)
-    x1 = int((box[0] + ((box[2]) / 2)) * img_width)
-    y1 = int((box[1] + ((box[3]) / 2)) * img_height)
+    x0 = int((box[0] - ((box[2]*1.4) / 2)) * img_width)
+    y0 = int((box[1] - ((box[3]*1.4) / 2)) * img_height)
+    x1 = int((box[0] + ((box[2]*1.4) / 2)) * img_width)
+    y1 = int((box[1] + ((box[3]*1.4) / 2)) * img_height)
     return [x0, y0, x1, y1]
 
 @torch.no_grad()
 def load_model(weights="",  # model.pt path(s)
         data='data/coco128.yaml',  # dataset.yaml path
-        imgsz=[512, 512],  # inference size (height, width)
+        imgsz=[640, 640],  # inference size (height, width)
         device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
         classes=None,  # filter by class: --class 0, or --class 0 2 3
         dnn=False,  # use OpenCV DNN for ONNX inference
@@ -45,9 +45,9 @@ def load_model(weights="",  # model.pt path(s)
 def detect_plate(model,
         device,
         source,  # file/dir/URL/glob, 0 for webcam
-        imgsz=[768,768],  # inference size (height, width)
-        conf_thres=0.04,  # confidence threshold
-        iou_thres=0.3,  # NMS IOU threshold
+        imgsz=[640,640],  # inference size (height, width)
+        conf_thres=0.001,  # confidence threshold
+        iou_thres=0.0001,  # NMS IOU threshold
         max_det=1000,  # maximum detections per image
         classes=None,  # filter by class: --class 0, or --class 0 2 3
         agnostic_nms=False,  # class-agnostic NMS
@@ -96,39 +96,45 @@ def detect_plate(model,
                 line= [float(value) if i!=0 else int(value) for i,value in enumerate(line)]
                 cls=line[0]
                 box=convert_box(line[1:],im0.shape[1],im0.shape[0])
-                box_image.append(box)
+                if box[0] > int(im0.shape[1]/5):
+                    if box[2] < int(im0.shape[1]*0.82):
+                        box_image.append(box)
     return box_image
+def crop_box(img_ori, box_img,img_output, check_crop):
+    img = img_ori
+    if len(box_img)!= 0:
+        for i in range(len(box_img)):
+            croped = img_ori[box_img[i][1]:box_img[i][3], box_img[i][0]: box_img[i][2]]
+            croped = cv2.resize(croped,(int(croped.shape[1]*1000/croped.shape[0]), 1000))
+            img = cv2.rectangle(img_ori, (box_img[i][0],box_img[i][1]), (box_img[i][2],box_img[i][3]), (0,0,255), 2)
+            if check_crop == True:
+                cv2.imwrite(img_output , croped)
+    return img
+
 
 def getMemoryUsage():
     usage = nvsmi.DeviceQuery("memory.used")["gpu"][0]["fb_memory_usage"]
     return "%d %s" % (usage["used"], usage["unit"])
 
-
-
 if __name__ == "__main__":
-    torch.cuda.empty_cache()
-    total_memory = torch.cuda.get_device_properties(0).total_memory
+    # total_memory = torch.cuda.get_device_properties(0).total_memory
     # tmp_tensor = torch.empty(int(total_memory * 0.499), dtype=torch.int8, device='cuda')
-    folder_img = "/home/anlab/Downloads/薬品複数画像/û≥òiòíÉöëµæ£/"
-    folder_output = "/home/anlab/Desktop/Songuyen/PIl_detection/box_output/"
+    folder_img = "/media/anlab/0e731fe3-5959-4d40-8958-e9f6296b38cb/home/anlab/songuyen/Download/train_pilll_1310/train/"
+    folder_output = "/home/anlab/Desktop/Songuyen/PIl_detection/check/"
     weight = "/home/anlab/Desktop/Songuyen/PIl_detection/CP/cp_0610v2.pt"
-    
     model, device = load_model(weights=weight)
     print("GPU Memory_____: %s" % getMemoryUsage())
-
-    torch.cuda.set_per_process_memory_fraction(0.5, device)
-
-    for path in os.listdir(folder_img):
-
-        img_ori = cv2.imread(folder_img + path)
-        box_img = detect_plate(model, device, img_ori)
-
-        # print("GPU Memory: %s" % getMemoryUsage())
-        print(len(box_img))
-        for i in range(len(box_img)):
-            croped = img_ori[box_img[i][1]:box_img[i][3], box_img[i][0]: box_img[i][2]]
-            isExist = os.path.exists(folder_output + path[:-4] )
-            if not isExist:
-                os.makedirs(folder_output + path[:-4])
-            cv2.imwrite(folder_output + path[:-4] + '/croped' + str(i) + ".jpg" , croped)
-            
+    count = 0
+    tt = 0
+    for pa in os.listdir(folder_img):
+        for pat in os.listdir(folder_img + pa ):
+            for path in os.listdir(folder_img + pa + "/" + pat):
+                img_ori = cv2.imread(folder_img + pa + "/" + pat + "/" + path)
+                center = img_ori.shape
+                dis = abs(center[0] - center[1])
+                img_ori = img_ori[int(dis/2):int(dis/2 + center[1]), 0: center[1]]
+                tt+=1
+                box_img = detect_plate(model, device, img_ori,iou_thres = 0.0001)
+                img_output = folder_output +  pa + pat +  path[:-4] + 'croped' + str(tt) + ".jpg"
+                img = crop_box(img_ori, box_img, img_output, check_crop = False)
+    print(tt)
